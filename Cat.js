5fbe9880a32620d99c847e3575bbcbd4c8bc9be1
Ly Nguyen Khang <nguyenkhang@Lys-MacBook-Pro.local>
2020-04-24 23:33:50 +0700

    Test error Cat eat Dog

--- Cat.js
+++ Cat.js
@@ -1,13 +1,20 @@
-
+var Mouse = require('./Mouse')
 var chalk = require('chalk')
 function Cat(name){
 	this.name = name;
 	this.stomach = [];
 };
 
-Cat.prototype.eat = function(mouse){
-	this.stomach.push(mouse);
+Cat.prototype.eat = function(animal){
+
+	if (animal instanceof Mouse){
+		this.stomach.push(animal);
+	} else {
+		throw new Error('Can can only eat mouse !');
+	}
 }
+
+
 
 Cat.prototype.sayHi = function() {
 	console.log('Hi! My name is ' + chalk.yellow(this.name))
